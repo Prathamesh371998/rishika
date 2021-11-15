10b1d8480aa277686dcf063c46d0b6932d5fe51c
Prathamesh371998 <prathameshsapkal544@gmail.com>
2021-11-15 21:53:13 +0530

    push by rishika

---
+++ src/test/java/org/sample/Company.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class Company {
+public void method2() {
+	// TODO Auto-generated method stub
+
+}
+public void method3() {
+	// TODO Auto-generated method stub
+
+}
+public void method4() {
+	// TODO Auto-generated method stub
+
+}
+}
