2eca8e509f6dcb9bc51f9867d91d63e998389a2a
Prathamesh371998 <prathameshsapkal544@gmail.com>
2021-11-15 22:52:27 +0530

    code by dinesh

---
+++ src/test/java/org/sample/Happy.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class Happy {
+private void method4() {
+	// TODO Auto-generated method stub
+
+}
+private void aus() {
+	// TODO Auto-generated method stub
+
+}
+private void ban() {
+	// TODO Auto-generated method stub
+
+}
+}
