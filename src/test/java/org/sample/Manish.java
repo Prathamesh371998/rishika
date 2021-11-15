2318fafb4414818743f8148da99e0f72271d6b70
Prathamesh371998 <prathameshsapkal544@gmail.com>
2021-11-15 23:05:28 +0530

    push by sayali

---
+++ src/test/java/org/sample/Manish.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class Manish {
+private void aus() {
+	// TODO Auto-generated method stub
+
+}
+private void ban() {
+	// TODO Auto-generated method stub
+
+}
+private void ladak() {
+	// TODO Auto-generated method stub
+
+}
+}
